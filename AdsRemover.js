3864cff40afca7c677562636956f0f59072112ee
Ray Zheng <zhrud21@gmail.com>
2015-04-21 18:54:41 -0700

    fix bug and update black/whitelist

--- AdsRemover.js
+++ AdsRemover.js
@@ -1,3 +1,5 @@
+var whiteList=['weibo'];
+var blackList=['bdstatic.com','cb.baidu.com','pos.baidu.com','baidustatic.com'];
 //load JQuery if needed
 (function() {
     // Load the script
@@ -18,20 +20,18 @@
 
     // Start polling...
     checkReady(function($) {
-    	whiteList=['weibo'];
-		blackList=['bdstatic.com','cb.baidu.com','pos.baidu.com'];
 		function removeIframes(){
-			for(var whiteKey in whiteList){
-				if ($(this).attr('src') && $(this).attr('src').substring(0,50).indexOf(whiteKey)>-1) {
-					console.log('Skip iFrame with src:'+$(this).attr('src').substring(0,50)+" "+ $(this).attr('src').substring(0,50).indexOf(whiteKey));
+			for(var i in whiteList){
+				if ($(this).attr('src') && $(this).attr('src').substring(0,50).indexOf(whiteList[i])>-1) {
+					console.log('Skip iFrame with src:'+$(this).attr('src').substring(0,50)+" contains whiteKey:"+whiteList[i]+" at:"+ $(this).attr('src').substring(0,50).indexOf(whiteList[i]));
 					return;
 				};
 			}
 			$(this).remove();
 		}
 		function removeImgContainKeyinBlackList(){
-			for(var key in blackList){
-				if($(this).attr('src').indexOf(key)>-1){
+			for(var i in blackList){
+				if($(this).attr('src').indexOf(blackList[i])>-1){
 					$(this).remove();
 					return;
 				}
